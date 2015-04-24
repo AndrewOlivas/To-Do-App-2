2877e125da2d16dc2c46217acddee14d1e897e07
Andrew Olivas <andrewrolivas@gmail.com>
2015-04-24 08:34:53 -0700

    milestone 9

--- index.php
+++ index.php
@@ -18,4 +18,21 @@
 	</form>
 	</div>
 </body>
+<script src="hhtp://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task(); 
+
+	function add_task(){
+		$('.add-new-task').submit(function() {
+			var new_task = $('.add-new-task input[name=new-task').val();
+
+			if (new_task != '') {
+					$.post('includes/add-task.php', { task: new_task}, function(data) {
+						$(('add-new-task input[name=new-task']).val();)
+					})
+			};
+		})
+	}
+</script>
+
 </html>
