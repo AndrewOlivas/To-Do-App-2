0e79648401a38e93b217c477ba1e02dada8c22f9
Andrew Olivas <andrewrolivas@gmail.com>
2015-04-24 09:17:16 -0700

    milestone 15 done

--- index.php
+++ index.php
@@ -9,7 +9,24 @@
 <body>
 	<div class="wrap">
 		<ul>
-			<php require("includes/connect.php"); ?>
+			<?php require("includes/connect.php"); 
+			$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+			$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
+			if ($results = $mysqli->query($query)) {
+				$numrows = $result->num_rows;
+				if ($numrows>0) {
+					while ($row = $results->fetch_assoc()) {
+						$task_id = $row['id'];
+						$task_name = $row['task'];
+
+						echo "<li>
+						<span>'.$task_name'
+						";
+					}
+				}
+			}
+
+			?>
 		</ul>
 	
 	</div>
