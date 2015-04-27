aa4ec4d7ebbb7f555e642aa3f9f942b4f3d3cb05
Andrew Olivas <andrewrolivas@gmail.com>
2015-04-27 09:57:24 -0700

    login/register done but i dont know if it works!!

---
+++ login.php
@@ -0,0 +1,14 @@
+<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+		<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+		<title></title>
+		<nav>
+			<a class="link" href="index.php">Home</a>
+		</nav>
+</head>
+<?php
+    // pulls files from other folders
+    require_once(__DIR__ . "/view/login-form.php");
+    require_once(__DIR__ . "/view/footer.php");
---
+++ register.php
@@ -0,0 +1,15 @@
+<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+		<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+		<title></title>
+		<nav>
+			<a class="link" href="index.php">Home</a>
+		</nav>
+	</head>
+
+<?php
+    require_once(__DIR__ . "/view/header.php");
+    require_once(__DIR__ . "/view/register-form.php");
+    require_once(__DIR__ . "/view/footer.php");
