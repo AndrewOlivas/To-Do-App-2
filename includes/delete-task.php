216f9200eccf53b81033362415a69f64c0124f2a
Andrew Olivas <andrewrolivas@gmail.com>
2015-04-27 09:52:29 -0700

    milestone 18 complete

--- includes/delete-task.php
+++ includes/delete-task.php
@@ -1,1 +1,8 @@
-delete-task
+<?php
+	$task_id = strip_tags($_POST['id']);
+	require('connect.php');
+	$mysqli = new mysqli('localhost', 'root', 'root', 'tasks',);
+
+	if ($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")) {
+		# code...
+	}
